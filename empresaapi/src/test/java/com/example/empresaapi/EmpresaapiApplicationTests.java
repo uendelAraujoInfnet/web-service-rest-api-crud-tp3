cd57886e606f73f64481952e3fe551be157e7acf
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-09-07 11:48:39 -0400

    Adicionado o projeto inicial criado pelo spring initialize e gitignore ajustado

---
+++ empresaapi/src/test/java/com/example/empresaapi/EmpresaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
